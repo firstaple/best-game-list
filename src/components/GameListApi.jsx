c689a510d72d537bf0a53b417ca1566c7baec3e5
firstaple <112137297+firstaple@users.noreply.github.com>
2023-01-09 16:12:07 +0900

    api 수정중

--- src/components/GameListApi.jsx
+++ src/components/GameListApi.jsx
@@ -5,7 +5,7 @@ const GameListApi = () => {
   const [loading, setLoading] = useState(true);
   const [games, setGames] = useState([]);
   const [api] = useState(
-    "https://api.rawg.io/api/games?key=bd1a96395fad40d3a2337b3ff3c01116&dates=2019-09-01,2019-09-30&platforms=18,1,7"
+    "https://api.rawg.io/api/games?key=bd1a96395fad40d3a2337b3ff3c01116&metacritic=80,100&page_size=40&"
   );
   const [nextApi, setNextApi] = useState();
   const [preApi, setPreApi] = useState();
